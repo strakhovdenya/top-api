fcf175acc15335abd751f363f3a48f403da3c2e5
Denis Strakhov <strakhov.denya@gmail.com>
2021-08-07 11:58:20 +0300

    ggg

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,7 +1,7 @@
 export class ReviewModel {
-    name: string;
-    title: string;
-    description: string;
-    raiting: number;
-    createdAt: Date;
+  name: string;
+  title: string;
+  description: string;
+  raiting: number;
+  createdAt: Date;
 }
