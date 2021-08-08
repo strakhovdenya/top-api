0b6d13a9977d8dfcc6f58f20dadee086403aabb0
Denis Strakhov <strakhov.denya@gmail.com>
2021-08-08 11:53:28 +0300

    create review service

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { prop } from "@typegoose/typegoose";
 import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { Types } from "mongoose";
 
 export interface ReviewModel extends Base { }
 
@@ -18,6 +19,5 @@ export class ReviewModel extends TimeStamps {
   raiting: number;
 
   @prop()
-  createdAt: Date;
+  productId: Types.ObjectId;
 }
-
