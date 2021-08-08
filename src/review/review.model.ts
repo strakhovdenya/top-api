ac37cd1fd4399d4a00084b5a82652ac07ab54ca4
Denis Strakhov <strakhov.denya@gmail.com>
2021-08-08 11:10:31 +0300

    add typegoose to models

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,23 @@
-export class ReviewModel {
-  _id: string;
+import { prop } from "@typegoose/typegoose";
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+
+export interface ReviewModel extends Base { }
+
+export class ReviewModel extends TimeStamps {
+
+  @prop()
   name: string;
+
+  @prop()
   title: string;
+
+  @prop()
   description: string;
+
+  @prop()
   raiting: number;
+
+  @prop()
   createdAt: Date;
 }
+
